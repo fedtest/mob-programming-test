206756f3ca97047f82ae13aaf967ed6aceff1f95
Ninos Malki <webteamfire@gmail.com>
2021-09-23 10:57:48 +0200

    added styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,23 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import styled from 'styled-components';
+
+const BodyStyled = styled.div`
+    background-color: mistyrose;
+    height: 100%;
+    text-align: center;
+    min-height: 100vh;
+`
 
 function App() {
-  const API_KEY = "";
-  const API_URL = "http://www.omdbapi.com/?apikey=65049324&s="; // s=star+wars
+    const API_KEY = "";
+    const API_URL = `http://www.omdbapi.com/?apikey=${API_KEY}&s=`; // s=star+wars
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BodyStyled>
+            MOBPROGRAMMERINGS TEST
+        </BodyStyled>
+    );
 }
 
 export default App;
