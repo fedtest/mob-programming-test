82acd0d9dd2a1e9a482fcafc58d15f6065675bef
Johan Mattsson <johan.mattsson@tele2.com>
2021-09-29 10:46:10 +0200

    WIP-MOB

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
 import React from "react";
 import "./App.css";
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const BodyStyled = styled.div`
-    background-color: mistyrose;
-    height: 100%;
-    text-align: center;
-    min-height: 100vh;
-`
+  background-color: mistyrose;
+  height: 100%;
+  text-align: center;
+  min-height: 100vh;
+`;
 
 function App() {
-    const API_KEY = "";
-    const API_URL = `http://www.omdbapi.com/?apikey=${API_KEY}&s=`; // s=star+wars
+  const API_KEY = "65049324";
+  const API_URL = `http://www.omdbapi.com/?apikey=${API_KEY}&s=star`; // s=star+wars
 
-    return (
-        <BodyStyled>
-            MOBPROGRAMMERINGSTEST
-        </BodyStyled>
-    );
+  const fetchMovies = async () => {
+    const response = await fetch(API_URL);
+
+  };
+
+  return <BodyStyled>MOBPROGRAMMERINGSTEST</BodyStyled>;
 }
 
 export default App;
