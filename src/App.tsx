bf8ff63377a8c4a6633fbe5fed67efffab0edf12
Ida Lindell <ida.lindell@tele2.com>
2021-09-29 10:58:32 +0200

    WIP-MOB

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import "./App.css";
 import styled from "styled-components";
 
@@ -13,6 +13,7 @@ function App() {
     useEffect(() => {
         fetchMovies()
     })
+    const [movies, setMovies] = useState([]);
 
   const API_KEY = "65049324";
   const API_URL = `http://www.omdbapi.com/?apikey=${API_KEY}&s=star`; // s=star+wars
@@ -20,15 +21,17 @@ function App() {
   const fetchMovies = async () => {
       try {
         const response = await fetch(API_URL);
-        const body = await response.json()
-        console.log(body);
-
+        const {Search} = await response.json()
+        console.log(Search);
+        setMovies(Search);
       } catch (error) {
         console.log(error);
       }
   };
 
-  return <BodyStyled>MOBPROGRAMMERINGSTEST</BodyStyled>;
+  return <BodyStyled>MOBPROGRAMMERINGSTEST
+      {movies.map((movie) => <li><ul></ul></li>)}
+  </BodyStyled>;
 }
 
 export default App;
