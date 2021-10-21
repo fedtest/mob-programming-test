f71206731887ca8f5a98361666a909f8186a6b93
Daniel <thedann8@gmail.com>
2021-10-21 15:49:01 +0200

    mob

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import React, { useState } from 'react';
 import './App.css';
 import styled from 'styled-components';
 
-interface response{
-    Search:[]
+interface response {
+  Search: [];
 }
 
 const BodyStyled = styled.div`
@@ -14,6 +14,14 @@ const BodyStyled = styled.div`
   min-height: 100vh;
 `;
 
+const Title = styled.h1`
+  color: red;
+`;
+
+const MovieTitle = styled.h2`
+  color: green;
+`;
+
 function App() {
   const API_KEY = '65049324';
   const API_URL = `http://www.omdbapi.com/?apikey=${API_KEY}&s=`; // s=star+wars
@@ -21,11 +29,8 @@ function App() {
   const [search, setSearch] = useState('');
   const [movieData, setMovieData] = useState<response>();
 
-  console.log(movieData?.Search);
-
   const handleClick = (input: string) => {
-
-    fetch(API_URL+input)
+    fetch(API_URL + input)
       .then((response) => response.json())
       .then((data) => setMovieData(data));
   };
@@ -36,6 +41,16 @@ function App() {
       <div>
         <input value={search} onChange={(e) => setSearch(e.target.value)} placeholder='skriv in din film' type='text'></input>
         <input value='sök' type='button' onClick={() => handleClick(search)} />
+      </div>
+      <div>
+        <Title>Sök resultat:</Title>
+        <ul>
+          {movieData?.Search.map((movie : any) => (
+            <li>
+              <MovieTitle>{movie?.Title}</MovieTitle>
+            </li>
+          ))}
+        </ul>
       </div>
     </BodyStyled>
   );
