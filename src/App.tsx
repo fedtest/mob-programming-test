056924c06eb59a08f54902c04318a21506a46206
Daniel <thedann8@gmail.com>
2021-10-21 15:24:27 +0200

    mob

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,38 @@
-import React from "react";
+import React, { useState } from 'react';
 
-import "./App.css";
+import './App.css';
 import styled from 'styled-components';
 
 const BodyStyled = styled.div`
-    background-color: mistyrose;
-    height: 100%;
-    text-align: center;
-    min-height: 100vh;
-`
+  background-color: mistyrose;
+  height: 100%;
+  text-align: center;
+  min-height: 100vh;
+`;
 
 function App() {
-    const API_KEY = "65049324";
-    const API_URL = `http://www.omdbapi.com/?apikey=${API_KEY}&s=`; // s=star+wars
+  const API_KEY = '65049324';
+  const API_URL = `http://www.omdbapi.com/?apikey=${API_KEY}&s=`; // s=star+wars
 
+  const [search, setSearch] = useState('');
 
-    const handleSubmit = (e:any) =>{
-        console.log(e.target.value);
-    }
-    return (
-        <BodyStyled>
-            
-            MOBPROGRAMMERINGSTEST
+  const handleClick = (input: string) => {
+    console.log(input);
 
-            <form onSubmit={handleSubmit(e)}>
-                <input  placeholder="skriv in din film" type='text'>
+    fetch('http://example.com/movies.json')
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  };
 
-                </input>
-                <input value="sök" type='button'/>
-            </form>
-        </BodyStyled>
-    );
+  return (
+    <BodyStyled>
+      MOBPROGRAMMERINGSTEST
+      <div>
+        <input value={search} onChange={(e) => setSearch(e.target.value)} placeholder='skriv in din film' type='text'></input>
+        <input value='sök' type='button' onClick={() => handleClick(search)} />
+      </div>
+    </BodyStyled>
+  );
 }
 
 export default App;
